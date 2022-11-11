4431204345bc0b691ae519275a78a93abb6cd9c8
Bryan M <bmaitland101@gmail.com>
2022-11-10 19:24:53 -0600

    new xref script

---
+++ R/xrefs.R
@@ -0,0 +1,13 @@
+
+# Xref table
+xref_taxa <- 
+  read_csv(here("data", "xref-spp.csv")) |> 
+  cleans_names_and_caps()
+
+xref_ports <- 
+  read_csv(here("data", "xref-sites-ports-regions.csv")) |> 
+  cleans_names_and_caps()
+
+xref_compartments <- 
+  read_csv(here("data", "xref_taxa_compartment.csv")) |> 
+  cleans_names_and_caps()
