59a9faf9a26cd85db8d9fb46115ebd9b260c5462
Bryan M <bmaitland101@gmail.com>
2022-12-29 13:26:33 -0600

    remove bad data / outliers

--- R/02_check-data.R
+++ R/02_check-data.R
@@ -1,9 +1,5 @@
 
-# Libraries, xrefs, and helpers
-source(here::here("R", "00_prep.R"))
-
-# Data
-load(here("out", "data", "compiled_data.RData"))
+# Check for outliter, etc. 
 
 
 # Summary ========================
@@ -21,6 +17,21 @@ data |> filter(is.na(length_mm)) |> count(compartment, species) |> print(n=Inf)
 
 
 
-# Plots  =======================================================
+## Outliers  ==================================================================
+
+data |>
+  filter(d13c < -10) |>
+  filter(d13c > -40) |>
+  ggplot(aes(d13c)) + geom_boxplot()
+data |> ggplot(aes(d15n)) + geom_boxplot()
+data |> ggplot(aes(cn)) + geom_boxplot()
+data |> ggplot(aes(length_mm)) + geom_boxplot()
+data |> ggplot(aes(mass_g)) + geom_boxplot()
+
+data <- data |> 
+  filter(d13c < -10) |> 
+  filter(d13c > -40) |> 
+  filter(cn < 30)
+
 
 
