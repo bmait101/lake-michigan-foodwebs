a6c75d2eedb44aee9e9debfa34f033ae4f259dd7
Bryan M <bmaitland101@gmail.com>
2023-01-17 16:21:59 -0600

    process reg model data

--- R/04_tp_process.R
+++ R/04_tp_process.R
@@ -40,15 +40,15 @@ load(file = here("out", "models", "tp", "tp_mods_2015.RData"))
 load(file = here("out", "models", "tp", "tp_mods_2015_ind.RData"))
 
 # Combine individual estimates with species levels
-tp_mods <- c(tp_mods_2015, tp_mods_2015_ind)
-data_subs <- c(data_subsets, data_subsets_ind)
+tp_mods_2015_all <- c(tp_mods_2015, tp_mods_2015_ind)
+data_subs_2015 <- c(data_subsets, data_subsets_ind)
 
 
 
 # Extract TP data from Parallel ===========================
 
 # Summarize TP data
-tp_data_2015 <- tp_mods |> map(summarize_TP_data)
+tp_data_2015 <- tp_mods_2015_all |> map(summarize_TP_data)
 
 str(tp_data_2015)
 tp_data_2015[[6]]
@@ -57,26 +57,52 @@ tp_data_2015[[6]]
 # Data for HBMs models  ===================================================
 
 # Map over data and tp data
-df_mods_2015 <- map2(data_subs, tp_data_2015, avg_mass_and_merge)
+df_reg_mod_data_2015 <- map2(data_subs_2015, tp_data_2015, avg_mass_and_merge)
 
 # Give names to list datasets
-names(df_mods_2015) <- c(
+names(df_reg_mod_data_2015) <- c(
   "scale01", "scale02a", "scale02b", "scale03a", "scale03b", 
   "scascale01_ind", "scascale02a_ind", "scascale03a_ind"
   )
 
 # Check it
-str(df_mods_2015)
+str(df_reg_mod_data_2015)
 
 
-# Need to process the scale variable in each list element
-df_mods_2015 %>%
-  map(~mutate(., NewColumn1 = first/(second*2), NewColumn2 = first/(second*3)))
+# Process the combined variables in each list element
+df_reg_mod_data_2015 <- df_reg_mod_data_2015 %>%
+  map(
+    ~rename(., lake_region = scale)) |> 
+  map(
+    ~separate(., lake_region, into = c("lake_region", "season"), sep = "_")) |> 
+  modify_if(function(y) any(is.na(y$season)), 
+            ~select(.x, -season)) |> 
+  modify_if(function(y) any(y$lake_region == "pooled"), 
+            ~select(.x, -lake_region)) |> 
+  modify_if(function(y) any(y$species == "alewife_north"), 
+            ~separate(.x, species, into = c("species", "lake_region"), sep = "_")) |> 
+  modify_if(function(y) any(y$species == "alewife_1"), 
+            ~separate(.x, species, into = c("species", "season"), sep = "_")) 
+
+# Convert factors
+df_reg_mod_data_2015[[2]]$lake_region <- factor(df_reg_mod_data_2015[[2]]$lake_region)
+df_reg_mod_data_2015[[3]]$lake_region <- factor(df_reg_mod_data_2015[[3]]$lake_region)
+df_reg_mod_data_2015[[4]]$lake_region <- factor(df_reg_mod_data_2015[[4]]$lake_region)
+df_reg_mod_data_2015[[4]]$season <- factor(df_reg_mod_data_2015[[4]]$season)
+df_reg_mod_data_2015[[5]]$lake_region <- factor(df_reg_mod_data_2015[[5]]$lake_region)
+df_reg_mod_data_2015[[5]]$season <- factor(df_reg_mod_data_2015[[5]]$season)
+df_reg_mod_data_2015[[7]]$lake_region <- factor(df_reg_mod_data_2015[[7]]$lake_region)
+df_reg_mod_data_2015[[8]]$lake_region <- factor(df_reg_mod_data_2015[[8]]$lake_region)
+df_reg_mod_data_2015[[8]]$season <- factor(df_reg_mod_data_2015[[8]]$season)
+
+str(df_reg_mod_data_2015)
+
+
 
 
 # Viz data for models =======================================
 
-df_mods_2015[[1]] |>
+df_reg_mod_data_2015[[1]] |>
   # filter(!(species %in% c("deepwater sculpin", "oligochaete", "round goby", "lake whitefish"))) |>
   ggplot(aes(Alpha_mode, TP_mode)) +
   # ggplot(aes(Alpha_mode, mass_g)) +
