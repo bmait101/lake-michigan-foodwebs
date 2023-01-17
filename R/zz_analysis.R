eee498a7dae001d5e6edc1e4e84ba350682350f6
Bryan M <bmaitland101@gmail.com>
2023-01-17 14:58:06 -0600

    rename scripts

--- R/zz_analysis.R
+++ R/zz_analysis.R
@@ -1,10 +1,18 @@
 
+# 1) calculate trophic position and alpha values for each species at different 
+# scales using the Two Baseline Full model approach; 
+# 2) Run hierarchical bayesian models with brms to explore the association 
+# between alpha and TP; 
+# 3) Compare models using different metrics (R2, LOO); and 
+# 4) Create some diagnostic plots.
+
+## Prep ============
+
 # for parallel processing
 cl <- parallel::makePSOCKcluster(parallel::detectCores())
 ncores <- 5
 
-
-## Prep data ============
+## Data ============
 
 df <- data |> 
   select(
@@ -12,20 +20,24 @@ df <- data |>
     compartment, species, species_group, 
     d13C = d13c_norm,  d15N = d15n, length_mm, mass_g
     ) |> 
-  drop_na(d13C, d15N, lake_region, year, season) |> 
-  # filter(dataset %in% c("csmi_2015", "nps_2015")) |>
-  filter(dataset %in% c("kornis_2014", "csmi_2015", "nps_2015", "glft_2016")) |>
+  rowid_to_column("ID") |> 
+  drop_na(d13C, d15N, lake_region, year, season) 
+
+df_01 <- df |> 
+  filter(dataset %in% c("csmi_2015", "nps_2015")) |>
+  # filter(dataset %in% c("kornis_2014", "csmi_2015", "nps_2015", "glft_2016")) |>
   # filter(dataset %in% c("csmi_2015","nps_2015","glft_2016","roth_2019")) |>
   filter(!compartment %in% c("ichthoplankton")) |>
   # filter(!compartment %in% c("ichthoplankton", "pom", "macro-alga")) |>
-  filter(!species %in% c("slimy sculpin", "deepwater sculpin")) |>
+  # filter(!species %in% c("slimy sculpin", "deepwater sculpin")) |>
   # mutate(lake_region = "pooled") |>
-  # mutate(lake_region = case_when(
-  #   lake_region %in% c("nw", "ne") ~ "north",
-  #   lake_region %in% c("sw", "se") ~ "south"
-  # )) |>
-  # mutate(lake_region = paste(lake_region, season, sep = "_")) |>
-  mutate(species = paste(species, year, lake_region, season, sep = "_")) |>
+  mutate(lake_region = case_when(
+    lake_region %in% c("nw", "ne") ~ "north",
+    lake_region %in% c("sw", "se") ~ "south"
+  )) |>
+  mutate(lake_region = paste(lake_region, season, sep = "_")) |>
+  # mutate(species = paste(species, year, lake_region, season, sep = "_")) |>
+  # mutate(species = paste(ID, species, sep = "_")) |>
   # filter(! lake_region %in% c("ne_1", "se_1")) |>
   mutate(trophic = case_when(
     compartment == "pom" ~ "b1", compartment == "macro-alga" ~ "b2", TRUE ~ "consumer"
@@ -38,15 +50,16 @@ df <- data |>
   droplevels() |> 
   as.data.frame()
 
-df |> 
+# Viz data
+df_01 |> 
   ggplot(aes(d13C, d15N)) + 
   geom_point(aes(color = trophic), size = 2, alpha = .5) + 
   facet_wrap(vars(lake_region)) +
   labs(color = "Trophic Group", shape = "Trophic Group")
 
 
-screenFoodWeb(df, grouping = c("species", "trophic"))
-summariseIsotopeData(df, grouping = c("species", "trophic"))
+# screenFoodWeb(df, grouping = c("species", "trophic"))
+# summariseIsotopeData(df, grouping = c("species", "trophic"))
 # ggsave(here("out", "plots", "TPdata_2015_pooled.png"),
 #        width = 12, height = 8, dpi = 300)
 
@@ -54,24 +67,27 @@ summariseIsotopeData(df, grouping = c("species", "trophic"))
 ## TP and alpha ===============
 
 IsotopesList <- extractIsotopeData(
-  df,
+  df_01,
   b1 = "b1",
   b2 = "b2", 
   baselineColumn = "trophic", 
   consumersColumn = "species",
-  # groupsColumn = "lake_region",
+  groupsColumn = "lake_region",
   d13C = "d13C", 
   d15N = "d15N"
 )
 
 summary(IsotopesList)
-plot(IsotopesList$`yellow perch_2016_ne_4`)
+plot(IsotopesList$`south-lake trout`)
+
+start_time <- Sys.time()
 
 # Run two baseline model (Increase n.iter, burn in, thin 4 model convergence)
 TP_model <- parLapply(
   cl, IsotopesList, multiModelTP,
   model = "twoBaselinesFull", lambda = 1,
-  print = TRUE, n.chains = 5, n.iter = 1000, burnin = 100, thin = 1
+  # print = TRUE, n.chains = 5, n.iter = 1000, burnin = 100, thin = 1
+  print = TRUE, n.chains = 5, n.iter = 100000, burnin = 10000, thin = 50
 ) 
 
 # save (TP_model, file = here("out", "models", "tp", "TP_model_corr-fish.RData"))
@@ -86,57 +102,46 @@ colnames(TP_data) <- c(
   'Alpha_lower','Alpha_upper','Alpha_median','Alpha_mode'
 )
 
-TP_data <- TP_data |> 
-  separate(species, into = c("species","year","lake_region","season"), sep = "_")
+TP_data <- TP_data |>
+  # separate(species, into = c("species","year","lake_region","season"), sep = "_") |> 
+  separate(group, into = c("lake_region", "season"), sep = "_")
+  
 
 # Data for models: average body size, merge TP data, and log transform body mass
-df_mod <- df %>%
+df_mod <- df_01 %>%
   filter(trophic == "consumer") %>%
-  select(species, mass_g) %>%
-  # select(species, lake_region, mass_g) %>%
-  separate(species, into = c("species","year","lake_region", "season"), sep = "_") %>%
+  # select(species, mass_g) %>%
+  select(species, lake_region, mass_g) %>%
+  separate(lake_region, into = c("lake_region", "season"), sep = "_") %>%
+  # separate(species, into = c("species","year","lake_region", "season"), sep = "_") %>%
   # group_by(species) %>%
-  group_by(species,year, lake_region, season) %>%
+  group_by(species, lake_region, season) %>%
   summarise(mass_g = mean(mass_g, na.rm = TRUE), .groups = 'drop') %>%
   # left_join(TP_data, by=c("species")) %>%
-  left_join(TP_data, by=c("species","year", "lake_region", "season")) %>%
+  left_join(TP_data, by=c("species", "lake_region", "season")) %>%
   mutate(
-    lake_region = factor(lake_region),
-    year = factor(year),
-    season = factor(season),
+    # lake_region = factor(lake_region),
+    # year = factor(year),
+    # season = factor(season),
     log_mass = log(mass_g)
   ) %>%
   drop_na(mass_g) |> 
-  as.data.frame ()
+  as.data.frame()
 
-# df_mod <- df %>%
-#   filter(trophic == "consumer") %>%
-#   # separate(lake_region, into = c("lake_region", "season"), sep = "_") |>
-#   select(species, lake_region, mass_g) %>%
-#   group_by(species, lake_region) %>%
-#   summarise(mass_g = mean(mass_g, na.rm = TRUE), .groups = 'drop') %>%
-#   # mutate(lake_region = paste(lake_region, season, sep = "_")) |>
-#   left_join(TP_data, by=c("species", "lake_region")) %>%
-#   # separate(lake_region, into = c("lake_region", "season"), sep = "_") |>
-#   mutate(
-#     lake_region = factor(lake_region),
-#     # season = factor(season),
-#     log_mass = log(mass_g)
-#   ) %>%
-#   as.data.frame ()
 
 # Plot data
 df_mod |> 
-  # ggplot(aes(Alpha_mode, TP_mode)) +
-  ggplot(aes(Alpha_mode, mass_g)) +
-  # ggplot(aes(log_length, TP_mode)) +
+  ggplot(aes(Alpha_mode, TP_mode)) +
+  # ggplot(aes(Alpha_mode, mass_g)) +
+  # ggplot(aes(mass_g, TP_mode)) +
   # geom_point(size = 3, alpha = 0.5, shape = 21) +
-  # geom_smooth(method = "lm", formula = "y ~ poly(x, 2)", color = "black") +
+  # geom_smooth(method = "lm", color = "black") +
   geom_smooth(method = "lm", formula = "y ~ poly(x, 2)", color = "black") +
-  geom_point(size = 3, alpha = 0.5) +
-  geom_point(aes(color = year, shape = lake_region), size = 3, alpha = 0.5) +
-  scale_y_log10(labels = scales::label_comma()) +
-  # ggrepel::geom_text_repel(aes(label = species), max.overlaps = 50, size=2) +
+  # geom_point(size = 3, alpha = 0.5) +
+  geom_point(aes(color = season, shape = lake_region), size = 3, alpha = 0.5) +
+  # scale_x_log10(labels = scales::label_comma()) +
+  # scale_y_log10(labels = scales::label_comma()) +
+  # ggrepel::geom_text_repel(aes(label = species), max.overlaps = 50, size=3) +
   # labs(title = "2015-2019, region baseline, by season",x = "Alpha", y = "TP") +
   # xlim(c(0,1)) +  
   # ylim(c(0.5,5)) + 
@@ -179,33 +184,33 @@ df_mod |>
 # )
 
 # Random effects
-model_structures <- list(
-  bf(TP_mode ~ log_mass * Alpha_mode + (1|lake_region) + (1|year/season)),
-  bf(TP_mode ~ log_mass + Alpha_mode + (1|lake_region)+ (1|year/season)),
-  bf(TP_mode ~ 1 + (1|lake_region)+ (1|year/season)),
+# model_structures <- list(
+#   bf(TP_mode ~ log_mass * Alpha_mode + (1|lake_region) + (1|year/season)),
+#   bf(TP_mode ~ log_mass + Alpha_mode + (1|lake_region)+ (1|year/season)),
+#   bf(TP_mode ~ 1 + (1|lake_region)+ (1|year/season)),
+# 
+#   bf(TP_mode ~ poly(Alpha_mode,2) + (1|lake_region)+ (1|year/season)),
+#   bf(TP_mode ~ poly(Alpha_mode,1) + (1|lake_region)+ (1|year/season)),
+#   bf(TP_mode ~ 1 + (1|lake_region)),
+# 
+#   bf(log_mass ~ poly(Alpha_mode,2) + (1|lake_region)+ (1|year/season)),
+#   bf(log_mass ~ poly(Alpha_mode,1) + (1|lake_region)+ (1|year/season)),
+#   bf(log_mass ~ 1 + (1|lake_region)+ (1|year/season))
+# )
 
-  bf(TP_mode ~ poly(Alpha_mode,2) + (1|lake_region)+ (1|year/season)),
-  bf(TP_mode ~ poly(Alpha_mode,1) + (1|lake_region)+ (1|year/season)),
+model_structures <- list(
+  bf(TP_mode ~ log_mass * Alpha_mode + (1|lake_region) + (1|season)),
+  bf(TP_mode ~ log_mass + Alpha_mode + (1|lake_region)),
   bf(TP_mode ~ 1 + (1|lake_region)),
 
-  bf(log_mass ~ poly(Alpha_mode,2) + (1|lake_region)+ (1|year/season)),
-  bf(log_mass ~ poly(Alpha_mode,1) + (1|lake_region)+ (1|year/season)),
-  bf(log_mass ~ 1 + (1|lake_region)+ (1|year/season))
-)
+  bf(TP_mode ~ poly(Alpha_mode,2) + (1|lake_region) + (1|season)),
+  bf(TP_mode ~ poly(Alpha_mode,1) + (1|lake_region) + (1|season)),
+  bf(TP_mode ~ 1 + (1|lake_region) + (1|season)),
 
-# model_structures <- list(
-#   bf(TP_mode ~ log_mass * Alpha_mode + (1|lake_region) + (1|season)),
-#   bf(TP_mode ~ log_mass + Alpha_mode + (1|lake_region)),
-#   bf(TP_mode ~ 1 + (1|lake_region)),
-#   
-#   bf(TP_mode ~ poly(Alpha_mode,2) + (1|lake_region) + (1|season)),
-#   bf(TP_mode ~ poly(Alpha_mode,1) + (1|lake_region) + (1|season)),
-#   bf(TP_mode ~ 1 + (1|lake_region) + (1|season)),
-#   
-#   bf(log_mass ~ poly(Alpha_mode,2) + (1|lake_region) + (1|season)),
-#   bf(log_mass ~ poly(Alpha_mode,1) + (1|lake_region) + (1|season)),
-#   bf(log_mass ~ 1 + (1|lake_region) + (1|season))
-# )
+  bf(log_mass ~ poly(Alpha_mode,2) + (1|lake_region) + (1|season)),
+  bf(log_mass ~ poly(Alpha_mode,1) + (1|lake_region) + (1|season)),
+  bf(log_mass ~ 1 + (1|lake_region) + (1|season))
+)
 
 # Fit models to data
 # names(model_structures) <- c("Quadratic","Linear", "Null")
@@ -220,6 +225,9 @@ for (i in 1:length (model_structures)){
   )
 }
 
+end_time <- Sys.time()
+
+end_time - start_time
 # save models
 # save(models, file="models_TP_alpha.R")
 # load("models_TP_alpha.R")
@@ -229,11 +237,11 @@ for (i in 1:length (model_structures)){
 # Model comparison =================
 # compare the different models using different metrics elpd, looic, r2
 
-models_sub <- models[1:2]
-comp <- loo_compare(
-  loo(models_sub[[1]]),
-  loo(models_sub[[2]])
-)
+# models_sub <- models[1:2]
+# comp <- loo_compare(
+#   loo(models_sub[[1]]),
+#   loo(models_sub[[2]])
+# )
 
 models_sub <- models[1:3]
 models_sub <- models[4:6]
@@ -293,6 +301,7 @@ model_sel_tab
 
 # select best model number (1, 2, or 3) for summaries and diagnostics
 best_model_num <- 1
+best_model_num <- 2
 
 
 # global grand mean - average predicted outcome ignoring group deviations
@@ -300,11 +309,14 @@ best_model_num <- 1
 
 conditional_effects(models_sub[[best_model_num]], re_formula = NULL)
 
+### 
 # TP_mode ~ log_mass * Alpha_mode --------
 expand_grid(
     Alpha_mode = c(0.2, 0.8),
     log_mass = modelr::seq_range(df_mod$log_mass, n = 100),
-    lake_region = NA
+    lake_region = NA, 
+    year = 2015, 
+    season = 1
     ) |>
   add_epred_draws(models_sub[[best_model_num]], re_formula = NULL) %>%  
   mutate(Alpha_mode = factor(Alpha_mode)) |>  
@@ -336,8 +348,8 @@ df_mod %>%
   as_tibble() |> 
   modelr::data_grid(
     Alpha_mode = modelr::seq_range(Alpha_mode, n = 100),
-    lake_region = "sw", 
-    year = 2015, 
+    lake_region = NA, 
+    # year = 2015, 
     season = 1
   ) |>
   add_epred_draws(models_sub[[best_model_num]], re_formula = NULL) %>%  
@@ -374,13 +386,13 @@ df_mod %>%
   as_tibble() |> 
   modelr::data_grid(
     Alpha_mode = modelr::seq_range(Alpha_mode, n = 100),
-    lake_region = "sw", 
+    lake_region = "south", 
     year = 2015, 
     season = 1
   ) |>
-  add_predicted_draws(models_sub[[best_model_num]], re_formula = NULL) %>%  
+  add_epred_draws(models_sub[[best_model_num]], re_formula = NULL) %>%  
   ggplot(aes(x = Alpha_mode)) +
-  stat_lineribbon(aes(y = .prediction), fill = "grey", .width = c(.95, .8, .5),alpha = 0.5) +
+  stat_lineribbon(aes(y = .epred), fill = "grey", .width = c(.95, .8, .5),alpha = 0.5) +
   # geom_point(
   #   data = df_mod, 
   #   # aes(x = Alpha_mode, y = log_mass, color = Alpha_mode),
@@ -434,7 +446,7 @@ pp_check(models_sub[[best_model_num]], ndraws=50)
 pp_check(
   models_sub[[best_model_num]], 
   type = "error_scatter_avg_vs_x", 
-  size = 1.1, x="length") +
+  size = 1.1, x="Alpha_mode") +
   stat_smooth(se = FALSE)
 pp_check(models_sub[[best_model_num]], type = "stat_2d")
 pp_check(models_sub[[best_model_num]], type = "loo_pit")
