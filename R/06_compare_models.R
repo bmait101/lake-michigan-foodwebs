8f1b44303ef45d39964e173e4fa3e58e8cb6ac20
Bryan M <bmaitland101@gmail.com>
2023-01-18 15:17:17 -0600

    update model comparison

--- R/06_compare_models.R
+++ R/06_compare_models.R
@@ -6,40 +6,40 @@ source(here::here("R", "00_prep.R"))
 load(file = here("out", "data", "reg_mod_data_2015.RData"))
 load(file = here("out", "models", "brms", "brm_mods_2015.RData"))
 
-df_reg_mod_data_2015
+names(reg_mod_data_2015)
 names(brm_mods_2015)
 
-brm_mods_2015[[8]] # this is list of 9 each is a model object, which is a list
+brm_mods_2015[[1]] # this is list each is a model object, which is a list
 str(brm_mods_2015[[1]][1]) # returns list of 1, which is list of 22
 str(brm_mods_2015[[1]][[1]]) # returns the model object, which is a list
 
-names(brm_mods_2015[[1]][[1]])
 
 # Model comparison 2015 =================
 
 # compare the different models using different metrics elpd, looic, r2
 # List 5 and 8 are the 'b' analyses (pooled baselines, estimated by grouping)
 
+
 # Scale 1
 models_sub <- brm_mods_2015[[1]][1:3]
 models_sub <- brm_mods_2015[[1]][4:6]
 models_sub <- brm_mods_2015[[1]][7:9]
 # Scale 1 - Ind
-models_sub <- brm_mods_2015[[2]][1:3]
+models_sub <- brm_mods_2015[[2]]
 
 # Scale 2
 models_sub <- brm_mods_2015[[3]][1:5]
 models_sub <- brm_mods_2015[[3]][6:10]
 models_sub <- brm_mods_2015[[3]][11:15]
 # Scale 2 - Ind
-models_sub <- brm_mods_2015[[4]][1:3]
+models_sub <- brm_mods_2015[[4]]
 
 # Scale 3
 models_sub <- brm_mods_2015[[6]][1:5]
 models_sub <- brm_mods_2015[[6]][6:10]
 models_sub <- brm_mods_2015[[6]][11:15]
 # Scale 3 - Ind
-models_sub <- brm_mods_2015[[7]][1:3]
+models_sub <- brm_mods_2015[[7]]
 
 
 comp <- loo_compare(
@@ -80,30 +80,16 @@ model_sel_tab$p_loo <- paste (comp_summary$p_loo %>% round(digits=2)," (",
 model_sel_tab$looic <- paste (comp_summary$looic %>% round(digits=2)," (",
                               comp_summary$se_looic %>% round(digits=2), ")",sep="")
 
-# count <- 1
-# r2_bayes(models_sub[[1]]) %>% round(digits=2)
-# r2_marg_cond <- r2_bayes(models_sub[[1]])
-# r2_marg_cond$R2_Bayes %>% round(digits=2)
-# 
-# loo_R2(models_sub[[3]]) %>% round(digits=2)
-# loo_R2(get("models_sub")[[3]]) %>% round(digits=2)
-
 for (i in 1:length(models_sub)){
   r2_marg_cond <- r2_bayes(models_sub[[i]])
   model_sel_tab[i,"r2_loo"] <- loo_R2(models_sub[[i]]) %>% round(digits=2)
-  model_sel_tab[i,"r2_marg"] <- 
-    # NA
-  paste (r2_marg_cond$R2_Bayes_marginal %>% round(digits=2)," (",attr(r2_marg_cond,"SE")$R2_Bayes_marginal %>% round(digits=2),")",sep="")
-  model_sel_tab[i,"r2_cond"] <- 
-    paste (r2_marg_cond$R2_Bayes %>% round(digits=2)," (",attr(r2_marg_cond,"SE")$R2_Bayes %>% round(digits=2),")",sep="")
+  # model_sel_tab[i,"r2_marg"] <- paste (r2_marg_cond$R2_Bayes_marginal %>% round(digits=2)," (",attr(r2_marg_cond,"SE")$R2_Bayes_marginal %>% round(digits=2),")",sep="")
+  model_sel_tab[i,"r2_cond"] <- paste (r2_marg_cond$R2_Bayes %>% round(digits=2)," (",attr(r2_marg_cond,"SE")$R2_Bayes %>% round(digits=2),")",sep="")
 }
 
 model_sel_tab
 
+# Dignostics -------------------------------------
 
-conditional_effects(models_sub[[1]], re_formula = NULL)
-conditional_effects(models_sub[[3]], re_formula = NULL)
-
-
-pp_check(models_sub[[1]], ndraws=100)
+pp_check(models_sub[[3]], ndraws=100)
 forest(models_sub[[1]],sort=FALSE)
