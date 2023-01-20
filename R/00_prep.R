05bb80a74fb1ba9dc4a4077b8ac12a9cd095ad7b
Bryan M <bmaitland101@gmail.com>
2023-01-20 12:36:15 -0600

    update dirs

--- R/00_prep.R
+++ R/00_prep.R
@@ -30,16 +30,16 @@ source(here("R", "helper_fxs.R"))  # helper functions for data cleaning
 
 # Source xrefs tables for ports, species
 xref_ports <- 
-  read_csv(here("data", "xref-sites-ports-regions.csv")) |> 
+  read_csv(here("data-raw", "xref-sites-ports-regions.csv")) |> 
   cleans_names_and_caps()
 
 xref_compartments <- 
-  read_csv(here("data", "xref_taxa_compartment.csv")) |> 
+  read_csv(here("data-raw", "xref_taxa_compartment.csv")) |> 
   cleans_names_and_caps()
 
 # load scientific names
 xref_sci_names <- 
-  read.csv(here("data", "xref_fish_species_names.csv")) |> 
+  read.csv(here("data-raw", "xref_fish_species_names.csv")) |> 
   mutate(common_name = str_to_lower(common_name))
 
 
