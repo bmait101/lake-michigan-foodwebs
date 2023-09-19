2d15b57959eaef9fdc89821c8e78fba4e94bed9b
Bryan M <bmaitland101@gmail.com>
2023-09-19 12:07:53 -0500

    reanalysis name fixes

--- R/10_param_estimates.R
+++ R/10_param_estimates.R
@@ -1,9 +1,11 @@
 
+# Script to extract parameter estimates from Bayesian regression models
+
 # model objects
 names(brm_mods_list)
 names(brm_mods_list_asym)
 
-# extract params 
+# extract parameter
 summary(brm_mods_list[["brm_mods_1"]][[1]])$fixed |> 
   rownames_to_column(var = 'Fixed Parameter') |> 
   mutate(across(where(is.numeric), ~round(., digits = 3))) |> 
@@ -19,40 +21,47 @@ make_param_tab <- function(model_object) {
     mutate(across(7:8, ~round(., digits = 0))) |> 
     rename_all(~gsub("\\.", " ", .)) |> 
     rename_all(~gsub("\\_", " ", .)) 
-  print(summary_tab)
+  summary_tab
 }
 
 # test
-make_param_tab(brm_mods_list[["brm_mods_1"]][[7]])
+make_param_tab(brm_mods_list[["brm_mods_1"]][[1]])
+make_param_tab(brm_mods_list[["brm_mods_2a_p2"]][[1]])
+make_param_tab(brm_mods_list_asym[["brm_mods_1_asym"]][[1]])
+
 
 # list best fit model objects to loop
 mods_for_tabs <- list(
-  brm_mods_list_asym[["brm_mods_list_asym"]][[2]],
+  brm_mods_list_asym[["brm_mods_1_asym"]][[2]],
   brm_mods_list[["brm_mods_1"]][[4]],
-  brm_mods_list[["brm_mods_1_ind"]][[1]],
+  brm_mods_list[["brm_mods_1_ind" ]][[1]],
   brm_mods_list[["brm_mods_1"]][[7]],
   
-  brm_mods_list_asym[["brm_mods_list_asym"]][[2]],
-  brm_mods_list[["brm_mods_2a_s1"]][[6]],
-  brm_mods_list[["brm_mods_2a_ind"]][[1]],
-  brm_mods_list[["brm_mods_2a_s1"]][[11]],
   
-  brm_mods_list_asym[["brm_mods_list_asym"]][[2]],
+  brm_mods_list_asym[["brm_mods_2b_s2_asym"]][[2]],
   brm_mods_list[["brm_mods_2b_s2"]][[6]],
-  brm_mods_list[["brm_mods_2b_ind"]][[1]],
+  brm_mods_list[["brm_mods_2b_s2_ind"]][[1]],
   brm_mods_list[["brm_mods_2b_s2"]][[11]],
   
-  brm_mods_list_asym[["brm_mods_list_asym"]][[1]],
-  brm_mods_list[["brm_mods_3a_s3"]][[6]],
-  brm_mods_list[["brm_mods_3a_s3"]][[1]],
-  brm_mods_list[["brm_mods_3a_s3"]][[11]],
+  brm_mods_list_asym[["brm_mods_2b_p4_asym"]][[1]],
+  brm_mods_list[["brm_mods_2b_p4"]][[6]],
+  brm_mods_list[["brm_mods_2b_p4_ind"]][[1]],
+  brm_mods_list[["brm_mods_2b_p4"]][[11]],
   
-  brm_mods_list_asym[["brm_mods_list_asym"]][[1]],
+  
+  brm_mods_list_asym[["brm_mods_3b_s4_asym"]][[1]],
   brm_mods_list[["brm_mods_3b_s4"]][[6]],
-  brm_mods_list[["brm_mods_3b_s4"]][[1]],
-  brm_mods_list[["brm_mods_3b_s4"]][[11]]
+  brm_mods_list[["brm_mods_3b_s4_ind"]][[1]],
+  brm_mods_list[["brm_mods_3b_s4"]][[11]], 
+
+  brm_mods_list_asym[["brm_mods_3b_p5_asym"]][[4]],
+  brm_mods_list[["brm_mods_3b_p5"]][[8]],
+  brm_mods_list[["brm_mods_3b_p5_ind"]][[1]],
+  brm_mods_list[["brm_mods_3b_p5"]][[11]]
+  
 )
 
+# Make tables and write to file
 mods_for_tabs[1:4] |>
   map_df(make_param_tab) |> 
   add_row(`Fixed Parameter` = "Asymmetric TP-body size relationship", .before = 1) |> 
@@ -63,12 +72,12 @@ mods_for_tabs[1:4] |>
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
     `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha2", 
+    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_params_scale01.csv"))
+  write_csv(here("out", "tbls", "brms_param_ests_scale01.csv"))
 
 
 mods_for_tabs[5:8] |>
@@ -81,29 +90,29 @@ mods_for_tabs[5:8] |>
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
     `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha2", 
+    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_params_scale02a.csv"))
+  write_csv(here("out", "tbls", "brms_param_ests_scale02b_s2.csv"))
 
 mods_for_tabs[9:12] |>
   map_df(make_param_tab) |> 
   add_row(`Fixed Parameter` = "Asymmetric TP-body size relationship", .before = 1) |> 
-  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP", .before = 5) |> 
-  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP (individual level)", .before = 9) |> 
-  add_row(`Fixed Parameter` = "Coupling of different energy pathways - Size", .before = 13) |> 
+  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP", .before = 6) |> 
+  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP (individual level)", .before = 10) |> 
+  add_row(`Fixed Parameter` = "Coupling of different energy pathways - Size", .before = 14) |> 
   mutate(`Fixed Parameter` = case_when(
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
     `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha2", 
+    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_params_scale02b.csv"))
+  write_csv(here("out", "tbls", "brms_param_ests_scale02b_p4.csv"))
 
 
 mods_for_tabs[13:16] |>
@@ -116,28 +125,28 @@ mods_for_tabs[13:16] |>
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
     `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha2", 
+    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_params_scale03a.csv"))
+  write_csv(here("out", "tbls", "brms_param_ests_scale03b_s4.csv"))
 
 mods_for_tabs[17:20] |>
   map_df(make_param_tab) |> 
   add_row(`Fixed Parameter` = "Asymmetric TP-body size relationship", .before = 1) |> 
-  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP", .before = 6) |> 
-  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP (individual level)", .before = 10) |> 
-  add_row(`Fixed Parameter` = "Coupling of different energy pathways - Size", .before = 14) |> 
+  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP", .before = 5) |> 
+  add_row(`Fixed Parameter` = "Coupling of different energy pathways - TP (individual level)", .before = 9) |> 
+  add_row(`Fixed Parameter` = "Coupling of different energy pathways - Size", .before = 13) |> 
   mutate(`Fixed Parameter` = case_when(
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
     `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha2", 
+    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_params_scale03b.csv"))
+  write_csv(here("out", "tbls", "brms_param_ests_scale03b_p5.csv"))
 
 
