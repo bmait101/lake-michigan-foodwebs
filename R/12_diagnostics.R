4f5bce132be9499eb2c734d187494b83a90a0fea
Bryan M <bmaitland101@gmail.com>
2023-09-26 08:29:48 -0500

    new residual plots

--- R/12_diagnostics.R
+++ R/12_diagnostics.R
@@ -1,5 +1,5 @@
 
-# Model Diagnostics
+## Model Diagnostics
 
 # All statistical models are sets of assumptions about the data 
 # generating process, and estimation will be meaningless or misleading 
@@ -11,58 +11,53 @@
 # 4. Equal variance of errors (standardized residuals)
 # 5. Normality
 
-# Prep ==========================================
+## Prep
 library(bayesplot)
 color_scheme_set("brightblue")
 
-available_ppc()
-
+names(reg_mod_data_tidy)
 names(brm_mods_list_asym)
 names(brm_mods_list)
-names(reg_mod_data_tidy)
 
-mod <- brm_mods_list_asym[["brm_mods_3b_p5_asym"]][[1]]
-mod <- brm_mods_list[["brm_mods_3b_p5"]][[6]]
-mod <- brm_mods_list[["brm_mods_3b_s4"]][[6]]
+mod_asym <- brm_mods_list_asym[["brm_mods_3b_s4_asym"]][[1]]
+mod_asym <- brm_mods_list_asym[["brm_mods_3b_p5_asym"]][[1]]
 
-print(mod)
-posterior_summary(mod)
+mod_couple_3bs4 <- brm_mods_list[["brm_mods_3b_s4"]][[6]]  # best fit coupling model with specific baselines
+mod_couple_3bp5 <- brm_mods_list[["brm_mods_3b_p5"]][[8]]  # best fit coupling model with pooled baselines
 
+## Coupling - specific (3bs4) ==========================
 
-# PP checks ====================================================================
-
-# Graphical check ------------------------------------------
-pp_check(mod, type = "dens_overlay", ndraws=100)
-
-# no major systematic discrepancies of our data 
-# from what can be predicted from our model
-
-# PPC for the mean (it should always fit) ----------------------------------
-# pp_check(mod, type = "stat_grouped", stat = "mean", group = "Alpha_mode")
-
-# PPC for the maximum and minimum values ---------------------------------
-pp_check(mod, type = "stat_2d", stat = c("max", "min"))
+print(mod_couple_3bs4)
+# posterior_summary(mod_couple_3bs4)
 
 
-# Ribbon plot to check for outliers ------------------------------------
-pp_check(mod, type = "ribbon", y_draw = "both")
+### PP checks ====================================================================
+
+#### Graphical check
+pp_check(mod_couple_3bs4, type = "dens_overlay", ndraws=100)
+
+# No major systematic discrepancies of our data 
+# from what can be predicted from our model.
+# Though, the peak for Yrep is a bit lower than Y. 
+
+#### PPC for the mean (it should always fit)
+# pp_check(mod_couple_3bs4, type = "stat_grouped", stat = "mean", group = "Alpha_mode")
+
+#### PPC for the maximum and minimum values
+pp_check(mod_couple_3bs4, type = "stat_2d", stat = c("max", "min"))
+
+#### Ribbon plot to check for outliers
+pp_check(mod_couple_3bs4, type = "ribbon", y_draw = "both")
 
 
-# Residual plots =========================================
+### Residual plots =========================================
 
-# Residuals -------------------------
+#### Residuals
 
-# If the model fit the data well, the points should be 
-# scattered with no specific pattern. If you see that the SD of the residuals 
-# is not uniform, or the residuals have some non-linear relationships 
-# with the predictor, there can be some problems.
+pp_check(mod_couple_3bs4, type = "error_scatter_avg")  # Q-Q plot 
+pp_check(mod_couple_3bs4, type = "error_scatter_avg_vs_x", x="Alpha_mode")
 
-pp_check(mod, type = "error_scatter_avg")  # Q-Q plot (residuals are normally distributed?)
-pp_check(mod, type = "error_scatter_avg_vs_x", x="Alpha_mode")
-pp_check(mod, type = "error_scatter_avg_vs_x", x="log_mass")
-
-
-# Standardized residuals -------------------------
+#### Standardized residuals
 
 # There are no intrinsic way to standardize the residuals in Bayesian methods,
 # and it’s not necessary to standardize them. However, it’s easier to 
@@ -70,33 +65,43 @@ pp_check(mod, type = "error_scatter_avg_vs_x", x="log_mass")
 # residuals larger than 3 or 4 in standardized values. The plot below 
 # shows the standardized residuals against the predicted y values.
 
-# Pull residuals
-res_df <- mod$data %>% 
-  mutate(predict_y = predict(mod)[ , "Estimate"], 
-         std_resid = residuals(mod)[ , "Estimate"])
+# Get species and log mass values
+tmp <- reg_mod_data_tidy[["s4"]] |> 
+  select(species, log_mass)
 
-# Residuals vs fitted (look for non-linearity)
-ggplot(res_df, aes(predict_y, std_resid)) + 
+# Pull residuals and bind tmp data
+res_3bs4 <- mod_couple_3bs4$data |> 
+  mutate(predict_y = predict(mod_couple_3bs4)[ , "Estimate"], 
+         std_resid = residuals(mod_couple_3bs4)[ , "Estimate"])|> 
+  as_tibble() |> 
+  bind_cols(tmp)
+
+# Q-Q plot; residuals vs fitted (look for non-linearity)
+ggplot(res_3bs4, aes(predict_y, std_resid)) + 
   geom_point() + 
   stat_smooth(se = FALSE)
 
-# Some funneling is apparent in variances
-
 # Residuals vs. explanatory variables
-ggplot(res_df, aes(Alpha_mode, std_resid)) + 
-  geom_point() 
+p.resid.3bs4 <- res_3bs4 |> 
+  ggplot(aes(Alpha_mode, std_resid, color = log_mass)) + 
+  geom_hline(yintercept = 0, linetype = "dashed") + 
+  geom_point() +
+  labs(x="Alpha",y="Standardized residual",color="log mass",
+       title = "Residuals vs. alpha (specific baselines)") + 
+  theme_clean()
+p.resid.3bs4
 
-ggplot(res_df, aes(TP_mode, std_resid)) +
+ggplot(res_3bs4, aes(TP_mode, std_resid)) +
   geom_point()
 
-ggplot(res_df, aes(lake_region, std_resid)) +
+ggplot(res_3bs4, aes(lake_region, std_resid)) +
   geom_boxplot()
 
-ggplot(res_df, aes(season, std_resid)) +
+ggplot(res_3bs4, aes(season, std_resid)) +
   geom_boxplot()
 
 
-# Multicollinearity ===============================
+### Multicollinearity ===============================
 
 # Strictly speaking, multicollinearity is not an assumption of regression. 
 # However, especially in frequentist analysis, having predictors that are 
@@ -107,37 +112,112 @@ ggplot(res_df, aes(season, std_resid)) +
 # to think about using a stronger prior or combining some predictors.
 
 # posterior density of the coefficients to see how correlated they are
-pairs(mod, pars = "b", 
+pairs(mod_couple_3bs4, pars = "b", 
       off_diag_args =  # arguments of the scatterplots
         list(size = 0.5,  # point size
              alpha = 0.25))  # transparency
 
 
+## Coupling - pooled (3bp5) ==========================
 
-# Other PP checks =====================================================
- 
-pp_check(mod, type = "dens_overlay", ndraws=100)
-# pp_check(mod, type = "hist", ndraws = 11)
+print(mod_couple_3bp5)
+# posterior_summary(mod_couple_3bp5)
 
-# pp_check(mod, type = "scatter", ndraws = 10)
-pp_check(mod, type = "scatter_avg", ndraws = 100)  # Q-Q plot (residuals are normally distributed?)
+### PP checks ====================================================================
+
+#### Graphical check
+pp_check(mod_couple_3bp5, type = "dens_overlay", ndraws=100)
+
+# No major systematic discrepancies of our data 
+# from what can be predicted from our model.
+# Though, the peak for Yrep is a bit lower than Y. 
+
+#### PPC for the mean (it should always fit)
+# pp_check(mod_couple_3bp5, type = "stat_grouped", stat = "mean", group = "Alpha_mode")
+
+#### PPC for the maximum and minimum values
+pp_check(mod_couple_3bp5, type = "stat_2d", stat = c("max", "min"))
+
+#### Ribbon plot to check for outliers
+pp_check(mod_couple_3bp5, type = "ribbon", y_draw = "both")
+
+### Residual plots =========================================
+
+#### Residuals
+
+pp_check(mod_couple_3bp5, type = "error_scatter_avg")  # Q-Q plot 
+pp_check(mod_couple_3bp5, type = "error_scatter_avg_vs_x", x="Alpha_mode")
+
+#### Standardized residuals
+
+# There are no intrinsic way to standardize the residuals in Bayesian methods,
+# and it’s not necessary to standardize them. However, it’s easier to 
+# compare with frequentist results to flag cases with standardized
+# residuals larger than 3 or 4 in standardized values. The plot below 
+# shows the standardized residuals against the predicted y values.
+
+# Get species and log mass values
+tmp <- reg_mod_data_tidy[["p5"]] |> 
+  select(species, log_mass)
+
+# Pull residuals and bind tmp data
+res_3bp5 <- mod_couple_3bp5$data |> 
+  mutate(predict_y = predict(mod_couple_3bp5)[ , "Estimate"], 
+         std_resid = residuals(mod_couple_3bp5)[ , "Estimate"])|> 
+  as_tibble() |> 
+  bind_cols(tmp)
+
+# Q-Q plot; residuals vs fitted (look for non-linearity)
+ggplot(res_3bp5, aes(predict_y, std_resid)) + 
+  geom_point() + 
+  stat_smooth(se = FALSE)
+
+# Residuals vs. explanatory variables
+p.resid.3bp5 <- res_3bp5 |> 
+  ggplot(aes(Alpha_mode, std_resid, color = log_mass)) + 
+  geom_hline(yintercept = 0, linetype = "dashed") + 
+  geom_point() + 
+  labs(x="Alpha",y="Standardized residual",color="log mass", 
+       title = "Residuals vs. alpha (pooled baselines)") + 
+  theme_clean()
+p.resid.3bp5
+
+ggplot(res_3bp5, aes(TP_mode, std_resid)) +
+  geom_point()
+
+ggplot(res_3bp5, aes(lake_region, std_resid)) +
+  geom_boxplot()
+
+ggplot(res_3bp5, aes(season, std_resid)) +
+  geom_boxplot()
 
 
-# pp_check(mod, type = "intervals", ndraws = 100)
-pp_check(mod, type = "intervals", ndraws = 100, size = 1.5, fatten = 0)
-pp_check(mod, type = "intervals", ndraws = 100, size = 1.5, fatten = 0,  x = "Alpha_mode")
-pp_check(mod, type = "intervals", ndraws = 100, size = 1.5, fatten = 0,  x = "log_mass")
+### Multicollinearity ===============================
 
-# pp_check(mod, type = "stat")
-# pp_check(mod, type = "stat_2d")
-# pp_check(mod, type = "stat_freqpoly", ndraws = 100)
+# Strictly speaking, multicollinearity is not an assumption of regression. 
+# However, especially in frequentist analysis, having predictors that are 
+# strongly correlated can increase the uncertainty of the posterior 
+# distributions of the regression coefficients.
 
-# LOO PP checks ==========================================================
-pp_check(mod, type = "loo_intervals", ndraws = 100)
-pp_check(mod, type = "loo_pit_overlay", ndraws = 100)
-pp_check(mod, type = "loo_pit", ndraws = 100)
-pp_check(mod, type = "loo_pit_qq", ndraws = 100)
-pp_check(mod, type = "loo_ribbon", ndraws = 100)
+# If some coefficients are particularly strongly correlated, you may need 
+# to think about using a stronger prior or combining some predictors.
+
+# posterior density of the coefficients to see how correlated they are
+pairs(res_3bp5, pars = "b", 
+      off_diag_args =  # arguments of the scatterplots
+        list(size = 0.5,  # point size
+             alpha = 0.25))  # transparency
 
 
+## Plots to go ===================
 
+p.resid.panel <- p.resid.3bs4 / p.resid.3bp5
+p.resid.panel <- p.resid.panel & 
+  plot_annotation(tag_levels = "a")
+
+path <- here::here("out", "plots", "residuals_coupling_panel")
+ggsave(glue::glue("{path}.pdf"), plot = p.resid.panel, 
+       width = 4, height = 4, scale = 1.5, device = cairo_pdf)
+pdftools::pdf_convert(pdf = glue::glue("{path}.pdf"),
+                      filenames = glue::glue("{path}.png"),
+                      format = "png", dpi = 300)
