f4698da5486d7ad622dc5768df41d072d415208d
bmait101 <bmaitland101@gmail.com>
2024-05-06 11:26:51 -0600

    clean up for revisions

--- R/10_param_estimates.R
+++ R/10_param_estimates.R
@@ -2,18 +2,22 @@
 
 # Prep =========================================================================
 
-source(here::here("R", "00_prep.R"))
+# libraries
+# source(here::here("R", "00_prep.R"))
+library(here)        # file paths
+library(tidyverse)   # ggplot2, dplyr, tidyr, readr, purrr, tibble, stringr, forcats
+library(brms)
 
 # Data
 load(file = here("out", "data", "reg_mod_data_v3.RData"))
 
 # Model objects
-load(file = here("out", "models", "brms", "brm_mods_list_v2.RData"))
-load(file = here("out", "models", "brms", "brm_mods_list_asym.RData"))
+load(file = here("out", "models", "brms", "brm_mods_list_20240327.RData"))
+# load(file = here("out", "models", "brms", "brm_mods_list_v2.RData"))
+# load(file = here("out", "models", "brms", "brm_mods_list_asym.RData"))
 
 # Check 
 names(brm_mods_list)
-names(brm_mods_list_asym)
 names(reg_mod_data_tidy)
 
 # Extract parameter estimates ==================================================
@@ -39,35 +43,32 @@ make_param_tab <- function(model_object) {
 
 # test
 make_param_tab(brm_mods_list[["brm_mods_1"]][[1]])
-make_param_tab(brm_mods_list[["brm_mods_2a_p2"]][[1]])
-make_param_tab(brm_mods_list_asym[["brm_mods_1_asym"]][[1]])
-
+make_param_tab(brm_mods_list[["brm_mods_2b_p4"]][[1]])
 
 # list best fit model objects to loop
 mods_for_tabs <- list(
-  brm_mods_list_asym[["brm_mods_1_asym"]][[2]],
+  brm_mods_list[["brm_mods_1"]][[2]],
   brm_mods_list[["brm_mods_1"]][[4]],
-  brm_mods_list[["brm_mods_1_ind" ]][[1]],
+  brm_mods_list[["brm_mods_1_ind"]][[1]],
   brm_mods_list[["brm_mods_1"]][[7]],
   
-  
-  brm_mods_list_asym[["brm_mods_2b_s2_asym"]][[2]],
+  brm_mods_list[["brm_mods_2b_s2"]][[2]],
   brm_mods_list[["brm_mods_2b_s2"]][[6]],
   brm_mods_list[["brm_mods_2b_s2_ind"]][[1]],
   brm_mods_list[["brm_mods_2b_s2"]][[11]],
   
-  brm_mods_list_asym[["brm_mods_2b_p4_asym"]][[1]],
+  brm_mods_list[["brm_mods_2b_p4"]][[1]],
   brm_mods_list[["brm_mods_2b_p4"]][[6]],
   brm_mods_list[["brm_mods_2b_p4_ind"]][[1]],
   brm_mods_list[["brm_mods_2b_p4"]][[11]],
   
   
-  brm_mods_list_asym[["brm_mods_3b_s4_asym"]][[1]],
+  brm_mods_list[["brm_mods_3b_s4"]][[1]],
   brm_mods_list[["brm_mods_3b_s4"]][[6]],
   brm_mods_list[["brm_mods_3b_s4_ind"]][[1]],
   brm_mods_list[["brm_mods_3b_s4"]][[11]], 
 
-  brm_mods_list_asym[["brm_mods_3b_p5_asym"]][[4]],
+  brm_mods_list[["brm_mods_3b_p5"]][[4]],
   brm_mods_list[["brm_mods_3b_p5"]][[8]],
   brm_mods_list[["brm_mods_3b_p5_ind"]][[1]],
   brm_mods_list[["brm_mods_3b_p5"]][[11]]
@@ -84,13 +85,12 @@ mods_for_tabs[1:4] |>
   mutate(`Fixed Parameter` = case_when(
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
+    `Fixed Parameter` == "IAlpha_modeE2" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_param_ests_scale01.csv"))
+  write_csv(here("out", "tbls","r1", "brms_param_ests_scale01.csv"))
 
 
 mods_for_tabs[5:8] |>
@@ -102,13 +102,12 @@ mods_for_tabs[5:8] |>
   mutate(`Fixed Parameter` = case_when(
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
+    `Fixed Parameter` == "IAlpha_modeE2" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_param_ests_scale02b_s2.csv"))
+  write_csv(here("out", "tbls","r1", "brms_param_ests_scale02b_s2.csv"))
 
 mods_for_tabs[9:12] |>
   map_df(make_param_tab) |> 
@@ -119,13 +118,12 @@ mods_for_tabs[9:12] |>
   mutate(`Fixed Parameter` = case_when(
     `Fixed Parameter` == "log_mass" ~ "Body mass", 
     `Fixed Parameter` == "Alpha_mode" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode21" ~ "Alpha", 
-    `Fixed Parameter` == "polyAlpha_mode22" ~ "Alpha^2", 
+    `Fixed Parameter` == "IAlpha_modeE2" ~ "Alpha^2", 
     TRUE ~ `Fixed Parameter`
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_param_ests_scale02b_p4.csv"))
+  write_csv(here("out", "tbls","r1", "brms_param_ests_scale02b_p4.csv"))
 
 
 mods_for_tabs[13:16] |>
@@ -143,7 +141,7 @@ mods_for_tabs[13:16] |>
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_param_ests_scale03b_s4.csv"))
+  write_csv(here("out", "tbls","r1", "brms_param_ests_scale03b_s4.csv"))
 
 mods_for_tabs[17:20] |>
   map_df(make_param_tab) |> 
@@ -160,38 +158,46 @@ mods_for_tabs[17:20] |>
   )) |> 
   mutate(across(everything(), as.character)) |> 
   mutate(across(everything(), ~replace_na(.x, " "))) |> 
-  write_csv(here("out", "tbls", "brms_param_ests_scale03b_p5.csv"))
+  write_csv(here("out", "tbls","r1", "brms_param_ests_scale03b_p5.csv"))
 
 
 
 ## Hypothesis tests ===============================================================
 
-# Pooled baselines
-brm_mods_list_asym[["brm_mods_3b_s4_asym"]][[1]] |> hypothesis("TP_mode > 0")
-brm_mods_list_asym[["brm_mods_3b_s4_asym"]][[1]] |> hypothesis("Alpha_mode < 0")
-brm_mods_list_asym[["brm_mods_3b_s4_asym"]][[1]] |> hypothesis("TP_mode:Alpha_mode > 0")
-
-brm_mods_list[["brm_mods_3b_s4"]][[6]] |> hypothesis("polyAlpha_mode21 > 0")
-brm_mods_list[["brm_mods_3b_s4"]][[6]] |> hypothesis("polyAlpha_mode22 < 0")
-
-brm_mods_list[["brm_mods_3b_s4_ind"]][[1]] |> hypothesis("polyAlpha_mode21 > 0")
-brm_mods_list[["brm_mods_3b_s4_ind"]][[1]] |> hypothesis("polyAlpha_mode22 < 0")
-
-brm_mods_list[["brm_mods_3b_s4"]][[11]] |> hypothesis("polyAlpha_mode21 < 0")
-brm_mods_list[["brm_mods_3b_s4"]][[11]] |> hypothesis("polyAlpha_mode22 < 0")
-
 # Specific baselines
-brm_mods_list_asym[["brm_mods_3b_p5_asym"]][[4]] |> hypothesis("TP_mode > 0")
-brm_mods_list_asym[["brm_mods_3b_p5_asym"]][[4]] |> hypothesis("Alpha_mode < 0")
+brm_mods_list[["brm_mods_3b_s4"]][[1]] |> hypothesis("Intercept < 0")
+brm_mods_list[["brm_mods_3b_s4"]][[1]] |> hypothesis("TP_mode > 0")
+brm_mods_list[["brm_mods_3b_s4"]][[1]] |> hypothesis("Alpha_mode < 0")
+brm_mods_list[["brm_mods_3b_s4"]][[1]] |> hypothesis("TP_mode:Alpha_mode > 0")
 
-brm_mods_list[["brm_mods_3b_p5"]][[8]] |> hypothesis("polyAlpha_mode21 > 0")
-brm_mods_list[["brm_mods_3b_p5"]][[8]] |> hypothesis("polyAlpha_mode22 < 0")
+brm_mods_list[["brm_mods_3b_s4"]][[6]] |> hypothesis("Intercept > 0")
+brm_mods_list[["brm_mods_3b_s4"]][[6]] |> hypothesis("Alpha_mode > 0")
+brm_mods_list[["brm_mods_3b_s4"]][[6]] |> hypothesis("IAlpha_modeE2 < 0")
 
-brm_mods_list[["brm_mods_3b_p5_ind"]][[1]] |> hypothesis("polyAlpha_mode21 > 0")
-brm_mods_list[["brm_mods_3b_p5_ind"]][[1]] |> hypothesis("polyAlpha_mode22 < 0")
+brm_mods_list[["brm_mods_3b_s4_ind"]][[1]] |> hypothesis("Intercept > 0")
+brm_mods_list[["brm_mods_3b_s4_ind"]][[1]] |> hypothesis("Alpha_mode > 0")
+brm_mods_list[["brm_mods_3b_s4_ind"]][[1]] |> hypothesis("IAlpha_modeE2 < 0")
 
-brm_mods_list[["brm_mods_3b_p5"]][[11]] |> hypothesis("polyAlpha_mode21 < 0")
-brm_mods_list[["brm_mods_3b_p5"]][[11]] |> hypothesis("polyAlpha_mode22 < 0")
+brm_mods_list[["brm_mods_3b_s4"]][[11]] |> hypothesis("Intercept < 0")
+brm_mods_list[["brm_mods_3b_s4"]][[11]] |> hypothesis("Alpha_mode > 0")
+brm_mods_list[["brm_mods_3b_s4"]][[11]] |> hypothesis("IAlpha_modeE2 < 0")
+
+# Pooled baselines
+brm_mods_list[["brm_mods_3b_p5"]][[4]] |> hypothesis("Intercept < 0")
+brm_mods_list[["brm_mods_3b_p5"]][[4]] |> hypothesis("TP_mode > 0")
+brm_mods_list[["brm_mods_3b_p5"]][[4]] |> hypothesis("Alpha_mode < 0")
+
+brm_mods_list[["brm_mods_3b_p5"]][[8]] |> hypothesis("Intercept > 0")
+brm_mods_list[["brm_mods_3b_p5"]][[8]] |> hypothesis("Alpha_mode > 0")
+brm_mods_list[["brm_mods_3b_p5"]][[8]] |> hypothesis("IAlpha_modeE2 < 0")
+
+brm_mods_list[["brm_mods_3b_p5_ind"]][[1]] |> hypothesis("Intercept > 0")
+brm_mods_list[["brm_mods_3b_p5_ind"]][[1]] |> hypothesis("Alpha_mode > 0")
+brm_mods_list[["brm_mods_3b_p5_ind"]][[1]] |> hypothesis("IAlpha_modeE2 < 0")
+
+brm_mods_list[["brm_mods_3b_p5"]][[11]] |> hypothesis("Intercept < 0")
+brm_mods_list[["brm_mods_3b_p5"]][[11]] |> hypothesis("Alpha_mode > 0")
+brm_mods_list[["brm_mods_3b_p5"]][[11]] |> hypothesis("IAlpha_modeE2 < 0")
 
 
 
